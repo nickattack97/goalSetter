d1741735205ee19a2b8620b85da41a2e40854566
nickattack97 <nickattack97@gmail.com>
2022-03-26 16:54:07 +0200

    backend complete commit

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,12 +1,13 @@
 const asyncHandler = require('express-async-handler')
 
 const Goal = require('../model/goalModel')
+const User = require('../model/userModel')
 
 // @desc Get goals
 // @route GET /api/goals
 // @access Private
 const getGoals = asyncHandler(async (req, res) => {
-    const goals = await Goal.find()
+    const goals = await Goal.find({ user: req.user.id })
 
     res.status(200).json(goals)
 })
@@ -22,6 +23,7 @@ const setGoals = asyncHandler(async (req, res) => {
 
     const goal = await Goal.create({
         text: req.body.text,
+        user: req.user.id
     })
 
     res.status(200).json(goal)
@@ -36,6 +38,19 @@ const updateGoals = asyncHandler(async (req, res) => {
     if(!goal){
         res.status(400)
         throw new Error('Goal not found')
+    }
+
+    const user = await User.findById(req.user.id)
+
+    //Check for user
+    if(!user){
+        res.status(400)
+        throw new Error('User not found')
+    }
+    //Make sure login user matches goal user
+    if(goal.user.toString() !== req.user.id){
+        res.status(400)
+        throw new Error('User not authorized')
     }
 
     const updatedGoal = await Goal.findByIdAndUpdate(req.params.id,req.body, {
@@ -55,6 +70,19 @@ const deleteGoals = asyncHandler(async (req, res) => {
     if(!goal){
         res.status(400)
         throw new Error('Goal not found')
+    }
+
+    const user = await User.findById(req.user.id)
+
+    //Check for user
+    if(!user){
+        res.status(401)
+        throw new Error('User not found')
+    }
+    //Make sure login user matches goal user
+    if(goal.user.toString() !== req.user.id){
+        res.status(401)
+        throw new Error('User not authorized')
     }
 
     await goal.remove()
--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -4,23 +4,31 @@ const User = require('../model/userModel')
 
 const protect = asyncHandler(async (req, res, next) => {
     let token
+
     if (
         req.headers.authorization &&
         req.headers.authorization.startsWith('Bearer')
     ) {
-        token = req.headers.authorization.split(' ')[1]
+        try {
+            //get token from header
+            token = req.headers.authorization.split(' ')[1]
+
+            //verify token
+            const decoded = jwt.verify(token, process.env.JWT_SECRET)
+           //get user from token
+           req.user = await User.findById(decoded.id).select('-password')
+        }
+        catch (error) {
+            console.log(error)
+            res.status(401)
+            throw new Error('Not authorized')
+        }
     }
     if (!token) {
-        return next(
-            new ErrorResponse(
-                'You are not logged in! Please log in to get access.',
-                401
-            )
-        )
+        res.status(401)
+        throw new Error('Not authorized, no token')
     }
-    const decoded = jwt.verify(token, process.env.JWT_SECRET)
-    req.user = await User.findById(decoded.id).select('-password')
     next()
 })
 
-module.exports = protect
+module.exports = { protect }

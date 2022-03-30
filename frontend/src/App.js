0643f91d6e25640f7104f36e96b56b27f95c15db
nickattack97 <nickattack97@gmail.com>
2022-03-30 13:10:47 +0200

    login authentication commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,6 @@
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import Header from './components/Header';
 import Dashboard from './pages/Dashboard';
 import Login from './pages/Login';
@@ -14,10 +16,10 @@ function App() {
           <Route path='/' element={<Dashboard/>}/>
           <Route path='/login' element={<Login/>}/>
           <Route path='/register' element={<Register/>}/>
-
         </Routes>
       </div>
     </Router>
+    <ToastContainer/>
     </>
   
   );
